d5b1a21ad2e2278bde50cc3da82d03bfe3fe3d0a
leartde <leartdell@gmail.com>
2024-12-15 23:02:06 +0100

    small cloudinaryManager cleanup

--- api/CloudinaryService/CloudinaryManager.cs
+++ api/CloudinaryService/CloudinaryManager.cs
@@ -5,7 +5,6 @@ namespace CloudinaryService;
 
 public class CloudinaryManager : ICloudinaryManager
 {
-    private readonly Cloudinary _cloudinary;
     private readonly Lazy<IImageUploader> _imageUploader;
     private readonly Lazy<IRawUploader> _rawUploader;
 
@@ -16,12 +15,12 @@ public class CloudinaryManager : ICloudinaryManager
             Environment.GetEnvironmentVariable("APIKEY"),
             Environment.GetEnvironmentVariable("APISECRET")
         );
-        _cloudinary = new Cloudinary(acc);
+        Cloudinary cloudinary = new Cloudinary(acc);
         _imageUploader = new Lazy<IImageUploader>(() => new
-            ImageUploader(_cloudinary)
+            ImageUploader(cloudinary)
         );
         _rawUploader = new Lazy<IRawUploader>(() => new
-            RawUploader(_cloudinary)
+            RawUploader(cloudinary)
         );
     }
 
